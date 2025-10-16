d47c2faac4684363ed680fd224a3e9ecbb439998
Elvenide <msffodomainer@gmail.com>
2025-10-16 17:49:01 -0400

    Implement CartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-    return cart.reduce((total, item) => total + calculateTotalCost(item), 0);
+    return cart?.reduce((total, item) => total + calculateTotalCost(item), 0) ?? 0;
   };
 
   const handleContinueShopping = (e) => {
@@ -65,7 +65,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
