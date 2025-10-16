bfab5b7427cd1f11ffbb5794834af3800164842a
Elvenide <msffodomainer@gmail.com>
2025-10-16 17:39:20 -0400

    Implement CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,26 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const existingQuantity = state.items.find(
+        (item) => item.name === action.payload.name
+      )?.quantity ?? 0;
+      state.items.push({
+        ...action.payload,
+        quantity: existingQuantity + 1,
+      });
     },
     removeItem: (state, action) => {
+      state.items.splice(
+        state.items.findIndex((item) => item.name === action.payload.name),
+        1
+      );
     },
     updateQuantity: (state, action) => {
-
-    
+      const item = state.items.find(
+        (item) => item.name === action.payload.name
+      );
+      if (item)
+        item.quantity = action.payload.quantity;
     },
   },
 });
